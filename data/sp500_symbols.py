478a3497a7553f266e536caea3daeaed8677694d
Ronethy <ron-keller@gmx.de>
2026-01-21 21:47:28 +0100

    Add SP500 symbols list to data/sp500_symbols.py

---
+++ data/sp500_symbols.py
@@ -0,0 +1,7 @@
+SP500_SYMBOLS = [
+    "AAPL","MSFT","NVDA","AMZN","META","GOOGL","TSLA","BRK.B",
+    "UNH","XOM","JPM","V","AVGO","LLY","HD","COST","AMD",
+    "NFLX","ADBE","CRM","INTC","PEP","KO","MRK","ABBV",
+    "CVX","WMT","DIS","MCD","CSCO","ORCL","IBM","QCOM",
+    "TXN","AMGN","GE","CAT","BA","GS","MS","RTX"
+]
